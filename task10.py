154bdf424964fa0c9cf1e4c5038fc8e41ba6fa38
navydragon <navydragon@inbox.ru>
2019-04-10 22:24:16 +0300

    Add files via upload

---
+++ task10.py
@@ -0,0 +1,9 @@
+def CoverWithTiles(length):
+    if length % 2 == 1 or length <= 0:
+        return -1
+    if length == 2:
+        return 3
+    elif length == 4:
+        return 11
+    else:
+        return 3 * CoverWithTiles(length - 2) + 2 * CoverWithTiles(length - 4)
